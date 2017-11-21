dbbdb4e09f283bf97e70115e6cf0162fc2db04d7
Gergely Fehérvári <fehergeri13@gmail.com>
2017-11-21 12:28:10 +0100

    fix tests

--- frontend/src/pages/editor/Editor.test.jsx
+++ frontend/src/pages/editor/Editor.test.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Editor from "./Editor";
+import {Provider} from "mobx-react";
 
 describe('Editor', () => {
     it('renders correctly', () => {
         const tree = renderer.create(
-            <Editor />
+            <Provider model={{}}><Editor /></Provider>
         ).toJSON();
         expect(tree).toMatchSnapshot();
     });
